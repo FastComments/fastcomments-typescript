f22cf73843cd58e911317971c6918d89d58c526f
winrid <winrid@gmail.com>
2021-01-11 08:53:48 -0800

    adding isLive flag for comment count widget

--- src/fast-comments-comment-count-config.ts
+++ src/fast-comments-comment-count-config.ts
@@ -3,6 +3,8 @@ export interface FastCommentsCommentCountConfig {
   tenantId: string
   /** Id that represents the page, if your comments aren't tied to the page url. Could be a URL or an ID (like an article id). */
   urlId?: string
-  /** Only render the number, not any text.. */
+  /** Only render the number, not any text. */
   numberOnly?: boolean
+  /** Set the count as being live. */
+  isLive?: boolean
 }
