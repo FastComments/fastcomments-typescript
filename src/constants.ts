cb52bd659f9bf4cb4525289216c5cd42328f6cff
winrid <winrid@gmail.com>
2023-12-05 21:17:30 -0800

    updating supported locales enum

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,30 @@
 export enum FastCommentsLocale {
+    bg_bg = 'bg_bg',
     zh_cn = 'zh_cn',
     zh_tw = 'zh_tw',
     hr_hr = 'hr_hr',
+    da_dk = 'da_dk',
     en_us = 'en_us',
     fr_fr = 'fr_fr',
     de_de = 'de_de',
+    el_cy = 'el_cy',
+    el_gr = 'el_gr',
+    he = 'he',
     it_it = 'it_it',
     ja_jp = 'ja_jp',
+    ko_kr = 'ko_kr',
     pl_pl = 'pl_pl',
     pt_br = 'pt_br',
     ru_ru = 'ru_ru',
+    ru_ua = 'ru_ua',
+    sr_ba = 'sr_ba',
+    sr_latn_rs = 'sr_latn_rs',
+    sl_sl = 'sl_sl',
+    sr_me = 'sr_me',
+    sr_rs = 'sr_rs',
     es_es = 'es_es',
-    tr_tr = 'tr_tr',
+    uk_ua = 'uk_ua',
+    tr_tr = 'tr_tr'
 }
 
 export enum FastCommentsSiteType {
